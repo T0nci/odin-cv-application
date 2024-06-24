69b446ef74a3c2740bf510842621a3ef3b569e0b
T0nci <138928016+T0nci@users.noreply.github.com>
2024-06-24 15:07:59 +0200

    feat: Create fields and state for Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,45 @@
 import "./Main.css";
 import AddSection from "./AddSection.jsx";
 import Preview from "./Preview.jsx";
+import { useState } from "react";
 
 export default function Main() {
+  const fields = {
+    generalInformation: ["name", "phone", "email"],
+    education: ["schoolName", "titleOfStudy", "location", "fromDate", "toDate"],
+    experience: [
+      "companyName",
+      "positionTitle",
+      "location",
+      "mainResponsibilities",
+      "fromDate",
+      "toDate",
+    ],
+  };
+
+  const [cvEntries, setCvEntries] = useState({});
+
   return (
     <main>
-      <AddSection heading="" components="" />
-      <Preview components="" />
-      <AddSection heading="" components="" />
-      <AddSection heading="" components="" />
+      <AddSection
+        heading="General Information"
+        cvEntries={cvEntries}
+        setCvEntries={setCvEntries}
+        fields={fields.generalInformation}
+      />
+      <Preview cvEntries={cvEntries} setCvEntries={setCvEntries} />
+      <AddSection
+        heading="Education"
+        cvEntries={cvEntries}
+        setCvEntries={setCvEntries}
+        fields={fields.education}
+      />
+      <AddSection
+        heading="Experience"
+        cvEntries={cvEntries}
+        setCvEntries={setCvEntries}
+        fields={fields.experience}
+      />
     </main>
   );
 }
