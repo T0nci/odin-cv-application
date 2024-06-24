30a5310253173d4efe5db7a132a5de62909ec0df
T0nci <138928016+T0nci@users.noreply.github.com>
2024-06-24 14:36:57 +0200

    feat: Create a helpers file for helper functions
    Also create function for turning strings from and to Camel Case.

---
+++ src/helpers.js
@@ -0,0 +1,29 @@
+function toCamelCase(name) {
+  if (typeof name !== "string") throw new Error("Expected string as argument!");
+
+  if (name.length < 1) return "";
+
+  return name
+    .toLowerCase()
+    .split(" ")
+    .map((val, index) => {
+      return index === 0 ? val : val[0].toUpperCase() + val.slice(1);
+    })
+    .join("");
+}
+
+function fromCamelCase(name) {
+  if (typeof name !== "string") throw new Error("Expected string as argument!");
+
+  if (name.length < 1) return "";
+
+  let newName = "";
+  for (let i = 0; i < name.length; i++) {
+    if (/[A-Z]/.test(name[i])) newName += " " + name[i].toLowerCase();
+    else newName += name[i];
+  }
+
+  return newName[0].toUpperCase() + newName.slice(1);
+}
+
+export { toCamelCase, fromCamelCase };
