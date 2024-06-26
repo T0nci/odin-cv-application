dfdc5723afa2552f1fbd860961454b30944a27f7
T0nci <138928016+T0nci@users.noreply.github.com>
2024-06-26 17:42:03 +0200

    feat: Create the forms for AddSection
    Create the form with uncontrolled inputs because having controlled
    inputs doesn't allow us to clear it in any way. Because with controlled
    input you can't use the reset method of a form.
    
    Used the uuid package to assign a unique unchangeable id because we
    can't use array ids since they will be changing with every filter and
    delete of an entry(On the other hand while creating the fields we use
    array indexes because they will be constant all the time).

---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import "./Form.css";
+import { fromCamelCase } from "../helpers.js";
+import { v4 as uuidV4 } from "uuid";
+
+export default function Form({ cvEntries, setCvEntries, fields }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const myFormData = new FormData(form);
+
+    const newEntry = {};
+    for (const [key, value] of myFormData) {
+      newEntry[key] = value;
+    }
+    newEntry.type = fields.type;
+    newEntry.key = uuidV4();
+
+    setCvEntries([...cvEntries, newEntry]); // Add object
+    form.reset();
+  };
+
+  // put handleSubmit on onSubmit form and not onSubmit button
+  return (
+    <form className="splitter" onSubmit={handleSubmit}>
+      {Object.entries(fields).map((field, index) => {
+        return (
+          <div key={index}>
+            <label>
+              {fromCamelCase(field[0])}:
+              <input
+                type={field[1]}
+                name={field[0]}
+                autoComplete="off"
+                required
+              />
+            </label>
+          </div>
+        );
+      })}
+      <div>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
+}
