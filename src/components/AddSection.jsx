7be9d159237feb55bd6dee68bf01817fbf728762
T0nci <138928016+T0nci@users.noreply.github.com>
2024-06-26 17:26:16 +0200

    feat: Add cleaning up info and props for children
    Also add an optional list if there are entries for that specific
    section.
    
    Also add a splitter class for clearer spacing.

--- src/components/AddSection.jsx
+++ src/components/AddSection.jsx
@@ -1,5 +1,25 @@
 import "./AddSection.css";
+import { fromCamelCase } from "../helpers.js";
+import Form from "./Form.jsx";
+import ItemsList from "./ItemsList.jsx";
 
-export default function AddSection() {
-  return <section className="add"></section>;
+export default function AddSection({
+  heading,
+  cvEntries,
+  setCvEntries,
+  fields,
+}) {
+  const sectionEntries = cvEntries.filter((entry) => {
+    return (entry.type = heading);
+  });
+
+  return (
+    <section className="add">
+      <h1>{fromCamelCase(heading)}</h1>
+      <Form cvEntries={cvEntries} setCvEntries={setCvEntries} fields={fields} />
+      {sectionEntries.length > 0 && (
+        <ItemsList sectionEntries={sectionEntries} displayName={heading} />
+      )}
+    </section>
+  );
 }
